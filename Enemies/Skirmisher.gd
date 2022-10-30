754f37dfb31a0a47c1fdbaf10d6f4f381f8f4741
Barzeboi <83527985+Barzeboi@users.noreply.github.com>
2022-10-29 21:30:59 -0500

    Update Skirmisher.gd

--- Enemies/Skirmisher.gd
+++ Enemies/Skirmisher.gd
@@ -1,19 +1,52 @@
-extends "res://Enemy.gd"
+extends KinematicBody2D
 
-var speed = 300
 
+var velocity = Vector2(0,0)
+var speed = 450
+export var health = 300
+onready var player = get_node("/root/TestMap/TestPlayer")
+export var sight_range = 350
+export var attack_range = 250
+
+
+enum {
+	PATROLLING
+	SKIRMISHING
+	ATTACKING
+	FLEEING
+}
+
+var state = PATROLLING
 
 func _physics_process(_delta):
-	velocity = Vector2.ZERO
-	if player:
-		velocity = position.direction_to(player.position) * speed
+	var distance = position.distance_to(player.global_position)
+	match state:
+		PATROLLING:
+			if (distance <= sight_range):
+				state = SKIRMISHING
+			else:
+				if (distance >= sight_range):
+					state = PATROLLING
+				elif (distance >= attack_range):
+					state = ATTACKING
+				elif (health <= 50):
+					state = FLEEING
+		SKIRMISHING:
+			velocity = (player.position - position).normalized() 
+			if (distance <= attack_range):
+				state = ATTACKING
+			else:
+				if (distance >= attack_range):
+					state = SKIRMISHING
+				elif (distance >= sight_range):
+					state = PATROLLING
+				elif (health < 50):
+					state = FLEEING
+		ATTACKING:
+			pass
+			if (health < 50):
+				state = FLEEING
+		FLEEING:
+			pass
 	velocity = move_and_slide(velocity)
 	
-
-func _on_Area2D_body_entered(_body):
-	player = _body
-		
-
-
-func _on_Area2D_body_exited(_body):
-	player = null
