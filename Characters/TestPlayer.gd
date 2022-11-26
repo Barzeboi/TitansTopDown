d6deac0cbc6a89b4deaf8412169ce686433acee1
Barzeboi <83527985+Barzeboi@users.noreply.github.com>
2022-11-26 04:29:07 -0600

    Added 18 lines of code

--- Characters/TestPlayer.gd
+++ Characters/TestPlayer.gd
@@ -2,83 +2,97 @@ extends KinematicBody2D
 
 const speed : int = 350
 var fire_rate = 9
-var Bullet = preload("res://Weapons/Bullet.tscn")
+var SMG = preload ("res://Weapons/SMG.tscn")
+var bullet = preload("res://Weapons/pellet.tscn")
 onready var update_delta = 1.0 / fire_rate
 var current_time : float = 0.0
 onready var _animated_sprite = $AnimatedSprite
 var last_direction = Vector2.ZERO
+enum{
+	NO_GUN
+	WITH_GUN
+}
+
+var state = NO_GUN
+
 
 func _physics_process(_delta):
 	
 
+	if PickUp():
+		state = WITH_GUN
+	
+	
 	var input_vector = Vector2(
 	Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 	Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	)
-	input_vector = input_vector.normalized()
-	move_and_slide(speed * input_vector)
-	
-	
-	if input_vector.length_squared() > 0:
-		# Store the direction for use later when needing
-		# to choose the idle animation
-		
-		# Diagonal movement
-		if input_vector.x != 0 and input_vector.y != 0:
+	var _input_vector = input_vector.normalized()
+	move_and_slide(speed * _input_vector)
 			
-			last_direction = input_vector
 			
-			if input_vector.y > 0:
-				if input_vector.x > 0:
-					_animated_sprite.play("run_down_right")
-				else:
-					_animated_sprite.play("run_down_left")
-			else:
-				if input_vector.x > 0:
-					_animated_sprite.play("run_up_right")
-				else:
-					_animated_sprite.play("run_up_left")
-		# Otherwise we are moving either horizontal or vertical, so its just a
-		# matter of finding out which.
-		# horizontal movement
-		elif input_vector.x != 0:
-			if input_vector.x > 0:
-				_animated_sprite.play("run_right")
-			else:
-				_animated_sprite.play("run_left")
-		# vertical movement
-		else:
-			if input_vector.y > 0:
-				_animated_sprite.play("run_down")
-			else:
-				_animated_sprite.play("run_up")
-	
-	else:
-		
-		if last_direction.x != 0 and last_direction.y != 0:
-			if last_direction.y > 0:
-				if last_direction.x > 0:
-					_animated_sprite.play("idle_down_right")
-				else:
-					_animated_sprite.play("idle_down_left")
+	match state:
+		NO_GUN:
+			if _input_vector.length_squared() > 0:
+			# Store the direction for use later when needing
+			# to choose the idle animation
+			
+			# Diagonal movement
+				if _input_vector.x != 0 and _input_vector.y != 0:
 					
-			else:
-				if last_direction.x > 0:
-					_animated_sprite.play("idle_up_right")
-				else:
-					_animated_sprite.play("idle_up_left")
+					last_direction = _input_vector
 					
-		elif last_direction.x != 0:
-			if last_direction.x > 0:
-				_animated_sprite.play("idle_left")
+					if _input_vector.y > 0:
+						if _input_vector.x > 0:
+							_animated_sprite.play("run_down_right")
+						else:
+							_animated_sprite.play("run_down_left")
+					else:
+						if _input_vector.x > 0:
+							_animated_sprite.play("run_up_right")
+						else:
+							_animated_sprite.play("run_up_left")
+				# Otherwise we are moving either horizontal or vertical, so its just a
+				# matter of finding out which.
+				# horizontal movement
+				elif _input_vector.x != 0:
+					if _input_vector.x > 0:
+						_animated_sprite.play("run_right")
+					else:
+						_animated_sprite.play("run_left")
+				# vertical movement
+				else:
+					if _input_vector.y > 0:
+						_animated_sprite.play("run_down")
+					else:
+						_animated_sprite.play("run_up")
+			
 			else:
-				_animated_sprite.play("idle_right")
 				
-		else:
-			if last_direction.y > 0:
-				_animated_sprite.play("idle_down")
-			else:
-				_animated_sprite.play("idle_up")
+				if last_direction.x != 0 and last_direction.y != 0:
+					if last_direction.y > 0:
+						if last_direction.x > 0:
+							_animated_sprite.play("idle_down_right")
+						else:
+							_animated_sprite.play("idle_down_left")
+							
+					else:
+						if last_direction.x > 0:
+							_animated_sprite.play("idle_up_right")
+						else:
+							_animated_sprite.play("idle_up_left")
+							
+				elif last_direction.x != 0:
+					if last_direction.x > 0:
+						_animated_sprite.play("idle_left")
+					else:
+						_animated_sprite.play("idle_right")
+						
+				else:
+					if last_direction.y > 0:
+						_animated_sprite.play("idle_down")
+					else:
+						_animated_sprite.play("idle_up")
 
 
 func _process(_delta: float) -> void:
@@ -90,7 +104,10 @@ func _process(_delta: float) -> void:
 		current_time = 0.0
 		shoot()
 		
+func PickUp():
+	pass
+		
 func shoot():
-	var b = Bullet.instance()
+	var b = bullet.instance()
 	owner.add_child(b)
-	b.transform = $Gun/Position2D.global_transform
+	b.transform = $Pivot/Position2D.global_transform
