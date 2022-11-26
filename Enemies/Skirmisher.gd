4b79e7a77387196c650bec45509f71f04813a3fb
Barzeboi <83527985+Barzeboi@users.noreply.github.com>
2022-11-26 04:30:33 -0600

    added 15 lines of code

--- Enemies/Skirmisher.gd
+++ Enemies/Skirmisher.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 
 
-var velocity = Vector2(0,0)
-var speed = 450
+var velocity 
+var speed = 250
+var skirm_speed = 150
 export var health = 300
 onready var player = get_node("/root/TestMap/TestPlayer")
 export var sight_range = 350
 export var attack_range = 250
+export var skirm_range = 100
 
-
+#Controls the states for AI Behavior. Do not delete, you dunce.
 enum {
 	PATROLLING
+	PURSUING
 	SKIRMISHING
 	ATTACKING
 	FLEEING
@@ -20,33 +23,44 @@ var state = PATROLLING
 
 func _physics_process(_delta):
 	var distance = position.distance_to(player.global_position)
+	var direction = position.direction_to(player.global_position)
+	var enemypos = position
+
 	match state:
 		PATROLLING:
 			if (distance <= sight_range):
-				state = SKIRMISHING
+				state = PURSUING
 			else:
-				if (distance >= sight_range):
-					state = PATROLLING
-				elif (distance >= attack_range):
-					state = ATTACKING
-				elif (health <= 50):
+				if (health <= 50):
 					state = FLEEING
-		SKIRMISHING:
-			velocity = (player.position - position).normalized() 
+		PURSUING:
+			velocity = move_and_slide(speed * direction)
 			if (distance <= attack_range):
 				state = ATTACKING
 			else:
-				if (distance >= attack_range):
-					state = SKIRMISHING
-				elif (distance >= sight_range):
+				if (distance >= sight_range):
+					state = PATROLLING
+				elif (health < 50):
+					state = FLEEING
+		SKIRMISHING:
+			velocity =  move_and_slide(skirm_speed * -direction)
+			if (distance >= skirm_range):
+				state = ATTACKING
+			else:
+				if (distance >= sight_range):
 					state = PATROLLING
 				elif (health < 50):
 					state = FLEEING
 		ATTACKING:
-			pass
+			velocity = move_and_slide(speed * direction * 0)
 			if (health < 50):
 				state = FLEEING
+			elif (distance <= skirm_range):
+				state = SKIRMISHING
+				
+			else:
+				if (distance >= attack_range):
+					state = PURSUING
 		FLEEING:
 			pass
-	velocity = move_and_slide(velocity)
 	
